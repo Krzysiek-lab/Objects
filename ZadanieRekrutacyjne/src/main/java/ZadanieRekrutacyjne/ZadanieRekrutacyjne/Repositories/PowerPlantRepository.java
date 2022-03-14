42729e91f7d68994bc48683b86ea8b52b3439ebe
Krzysiek-lab <krzyho.91@o2.pl>
2022-03-14 23:32:17 +0100

    2

--- ZadanieRekrutacyjne/src/main/java/ZadanieRekrutacyjne/ZadanieRekrutacyjne/Repositories/PowerPlantRepository.java
+++ ZadanieRekrutacyjne/src/main/java/ZadanieRekrutacyjne/ZadanieRekrutacyjne/Repositories/PowerPlantRepository.java
@@ -23,38 +23,38 @@ public interface PowerPlantRepository extends JpaRepository<PowerPlant, Integer>
 
     ////////////////
 //to nowe
-    @Query("select p from PowerPlants p where p.power in (select p from PowerPlant p GROUP BY p.power having " +
-            "count(p.power) >=2")
-    List<PowerPlant> getDuplicatePowerValues();
-
-//    @Query("select p from PowerPlant p GROUP BY p.power having count(p.power) >=2")
+//    @Query("select p from PowerPlants p where p.power in (select p from PowerPlant p GROUP BY p.power having " +
+//            "count(p.power) >=2")
 //    List<PowerPlant> getDuplicatePowerValues();
+
+    @Query("select p from PowerPlant p GROUP BY p.power having count(p.power) >=2")
+    List<PowerPlant> getDuplicatePowerValues();
 
 
     @Query("select p from PowerPlant p GROUP BY p.power having count(p.power) =1")
     List<PowerPlant> getDistinctPowerValues();
 
     //to nowe
-    @Query("select p from PowerPlants p where p.place in (select p from PowerPlant p GROUP BY p.place having " +
-            "count(p.place) >=2")
-    List<PowerPlant> getDuplicatePlaceValues();
-
-
-//    @Query("select p from PowerPlant p GROUP BY p.place having count(p.place) >=2")
+//    @Query("select p from PowerPlants p where p.place in (select p from PowerPlant p GROUP BY p.place having " +
+//            "count(p.place) >=2")
 //    List<PowerPlant> getDuplicatePlaceValues();
+
+
+    @Query("select p from PowerPlant p GROUP BY p.place having count(p.place) >=2")
+    List<PowerPlant> getDuplicatePlaceValues();
 
 
     @Query("select p from PowerPlant p GROUP BY p.place having count(p.place) =1")
     List<PowerPlant> getDistinctPlaceValues();
 
     //to nowe
-    @Query("select p from PowerPlants p where p.name in (select p from PowerPlant p GROUP BY p.name having " +
-            "count(p.name) >=2")
-    List<PowerPlant> getDuplicateNameValues();
-
-
-//    @Query("select p from PowerPlant p GROUP BY p.name having count(p.name) >=2")
+//    @Query("select p from PowerPlants p where p.name in (select p from PowerPlant p GROUP BY p.name having " +
+//            "count(p.name) >=2")
 //    List<PowerPlant> getDuplicateNameValues();
+
+
+    @Query("select p from PowerPlant p GROUP BY p.name having count(p.name) >=2")
+    List<PowerPlant> getDuplicateNameValues();
 
 
     @Query("select p from PowerPlant p GROUP BY p.name having count(p.name) =1")
